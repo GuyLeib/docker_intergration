78100f9d64f6a562cc86922b4007728d51926837
GuyLeib <guyleib9@gmail.com>
2023-01-11 14:32:03 +0200

    added multi processing ability.

--- neoantigens_prediction.py
+++ neoantigens_prediction.py
@@ -2,15 +2,10 @@ import os
 import docker
 import json
 from tqdm import tqdm
-with open("hla_dict.json", "r") as file:
-    hla_dict = json.load(file)
-client = docker.from_env()
-patient_list=list(hla_dict.keys())
-for patient in tqdm(patient_list):
-    patient_id = patient
-    alleles = hla_dict[patient_id]
-    alleles = ",".join(alleles)
-    command = "predict --runID {} --rundir ./test_neo/ --peptides sample_input_peptides.txt --alleles {}".format(patient_id, alleles)
+from concurrent.futures import ProcessPoolExecutor as Pool 
+
+def predict(command_var):
+    command = "predict --runID {} --rundir ./test_neo/ --peptides proteosome_output.txt --alleles {}".format(command_var[0], command_var[1])
     client.containers.run(
         "ssarkizova/hlathena-external",
         command=command,
@@ -19,3 +14,19 @@ for patient in tqdm(patient_list):
         stdin_open=True,
         tty=True
     )
+
+
+if __name__ == "__main__":     
+    with open("hla_dict.json", "r") as file:
+        hla_dict = json.load(file)
+    client = docker.from_env()
+    patient_list=list(hla_dict.keys())
+    command_list=[]
+    for patient_id in patient_list:
+        alleles = hla_dict[patient_id]
+        alleles = ",".join(alleles)
+        command_list.append((patient_id,alleles))
+
+
+    with Pool(max_workers=80) as pool:
+        pool.map(predict,command_list)
